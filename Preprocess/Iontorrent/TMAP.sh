c3ec1afc054c81276acf7bf3e789f22edc2ab226
leefall <leefall2@naver.com>
2022-07-23 18:21:44 +0900

    Preprocessing for Sequencing data

---
+++ Preprocess/Iontorrent/TMAP.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+
+~/tools/TMAP/TMAP/tmap mapall -f ~/mypro/Cancer_Panel_Package/Package/data/hg19.fasta -i fastq -r  /mnt/QNAP/leefall2/DeviCNV/raw/SRR5439528.fastq -v -Y -u -o 2 stage1 map4 > /storage/home/leefall2/mypro/DeviCNV/AlignedBam/SRR5439528.bam
+
+#echo "END"
+#StartDate= $( date )
+#NowDate=$(date+%m/%d/%H:$M)
+#echo $NowDate
+#echo $StartDate
+date
+
