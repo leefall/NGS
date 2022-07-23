3230528285ff6134a3a2e16facbe8b67b1e8a41f
leefall <leefall2@naver.com>
2022-07-23 18:28:29 +0900

    ISAAC Aligner

---
+++ Preprocess/illumina/ISAAC_Run.sh
@@ -0,0 +1,3 @@
+isaac-align -r /storage/home/leefall2/mypro/Cancer_Panel_Package/Package/data/hg19/sorted-reference.xml -b /storage/home/SNUH/S150047277 -m 100 --base-calls-format fastq-gz -o ./Result_S150047277
+isaac-align -r /storage/home/leefall2/mypro/Cancer_Panel_Package/Package/data/hg19/sorted-reference.xml -b /storage/home/SNUH/S150058928 -m 100 --base-calls-format fastq-gz -o ./Result_S150058928
+isaac-align -r /storage/home/leefall2/mypro/Cancer_Panel_Package/Package/data/hg19/sorted-reference.xml -b /storage/home/SNUH/S160066083 -m 100 --base-calls-format fastq-gz -o ./Result_S160066083
